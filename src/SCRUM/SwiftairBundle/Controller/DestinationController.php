26c1ebd6b60ec20a6a3a353de29c7c2eafeee497
karelVanGeerdeghom <karel.van.geerdeghom@gmail.com>
2015-01-25 12:34:11 +0100

    bestemming pagina form

--- src/SCRUM/SwiftairBundle/Controller/DestinationController.php
+++ src/SCRUM/SwiftairBundle/Controller/DestinationController.php
@@ -3,11 +3,31 @@
 namespace SCRUM\SwiftairBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use SCRUM\SwiftairBundle\Entity\Destination;
 
-class DestinationController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('SCRUMSwiftairBundle:Destination:destination.html.twig');
+class DestinationController extends Controller {
+    public function indexAction() {
+        $em = $this->getDoctrine()->getManager();
+        $landen = $em->getRepository('SCRUMSwiftairBundle:Landen')->findAll();
+
+        $destination = new Destination();
+        
+        $form = $this->createFormBuilder($destination)
+            ->setAction($this->generateUrl('scrum_swiftair_booking')) 
+            ->add('vertrek', 'choice', array( 'label' => 'Vertrek:', 'choices' => $this->getChoices($landen)))
+            ->add('bestemming', 'choice', array( 'label' => 'Bestemming:', 'choices' => $this->getChoices($landen)))
+            ->add('save', 'submit', array('label' => 'Zoek Vlucht'))
+            ->getForm();
+        
+        return $this->render('SCRUMSwiftairBundle:Destination:destination.html.twig', array('landen' => $landen, 'form' => $form->createView()));
+    }
+    
+    public function getChoices($landen) {
+        $choices = array();
+        $choices[0] = '--- Kies Land ---';
+        
+        foreach ($landen as $land) { $choices[$land->getId()] = $land->getNaam(); }
+        
+        return $choices;
     }
 }
--- src/SCRUM/SwiftairBundle/Controller/IndexController.php
+++ src/SCRUM/SwiftairBundle/Controller/IndexController.php
@@ -3,31 +3,9 @@
 namespace SCRUM\SwiftairBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use SCRUM\SwiftairBundle\Entity\Destination;
 
 class IndexController extends Controller {
     public function indexAction() {
-        $em = $this->getDoctrine()->getManager();
-        $landen = $em->getRepository('SCRUMSwiftairBundle:Landen')->findAll();
-        
-        $destination = new Destination();
-        
-        $form = $this->createFormBuilder($destination)
-            ->setAction($this->generateUrl('scrum_swiftair_booking')) 
-            ->add('vertrek', 'choice', array( 'label' => 'Vertrek:', 'choices' => $this->getChoices($landen)))
-            ->add('bestemming', 'choice', array( 'label' => 'Bestemming:', 'choices' => $this->getChoices($landen)))
-            ->add('save', 'submit', array('label' => 'Zoek Vlucht'))
-            ->getForm();
-
-        return $this->render('SCRUMSwiftairBundle:Index:index.html.twig', array('landen' => $landen, 'form' => $form->createView()));
-    }
-    
-    public function getChoices($landen) {
-        $choices = array();
-        $choices[0] = '--- Kies Land ---';
-        
-        foreach ($landen as $land) { $choices[$land->getId()] = $land->getNaam(); }
-        
-        return $choices;
+        return $this->render('SCRUMSwiftairBundle:Index:index.html.twig');
     }
 }
