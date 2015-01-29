c52dffbc389c3584c94a786763284abb902bf3d6
karelVanGeerdeghom <karel.van.geerdeghom@gmail.com>
2015-01-29 09:51:07 +0100

    vlucht gener test 2

--- vluchten-dummy-generator.php
+++ vluchten-dummy-generator.php
@@ -13,24 +13,26 @@ $aantalOpEenDag = 3;
 // print_r($vertrekmoment);
 
 
-$dbh = New PDO("mysql:localhost;dbname=swiftair","swiftair","123456");
+$dbh = New PDO("mysql:localhost;dbname=swiftair","swiftair",123456);
 
 for ($i=0;$i<$aantalDagen;$i++) {
 
-	for($j=1;$j<=$aantalOpEenDag;$j++) {
-		date_add($vertrekmoment, date_interval_create_from_date_string(( 6) . ' hours'));
-		$vertrekString = $vertrekmoment->format('Y-m-d H:i:s');
-		$aankomstString = $aankomstmoment->format('Y-m-d H:i:s');
- 		$sql = "INSERT INTO `vluchten`(`vertrekhavenid`, `aankomsthavenid`, `vliegtuigid`, `vertrekdatum`, `aankomstdatum`) VALUES ('".$vertrek."','".$aankomst."','".$vliegtuigid."','".($vertrekString)."','".$aankomstString."'')";
- 		print ($sql);
- 		$dbh->exec($sql);
- 		if ($j==$aantalOpEenDag) {
- 			date_add($vertrekmoment, date_interval_create_from_date_string('-18 hours'));
- 		}
-	}
-	date_add($vertrekmoment, date_interval_create_from_date_string(( 1) . ' days'));
+//	for($j=1;$j<=$aantalOpEenDag;$j++) {
+//		date_add($vertrekmoment, date_interval_create_from_date_string(( 6) . ' hours'));
+//		$vertrekString = $vertrekmoment->format('Y-m-d H:i:s');
+//		$aankomstString = $aankomstmoment->format('Y-m-d H:i:s');
+// 		$sql = "INSERT INTO `vluchten`(`vertrekhavenid`, `aankomsthavenid`, `vliegtuigid`, `vertrekdatum`, `aankomstdatum`) VALUES ('".$vertrek."','".$aankomst."','".$vliegtuigid."','".($vertrekString)."','".$aankomstString."')";
+// 		print ($sql."<br>");
+// 		$dbh->exec($sql);
+// 		if ($j==$aantalOpEenDag) {
+// 			date_add($vertrekmoment, date_interval_create_from_date_string('-18 hours'));
+// 		}
+//	}
+//	date_add($vertrekmoment, date_interval_create_from_date_string(( 1) . ' days'));
 }
-
+        $query = "SELECT * FROM vluchten LIMIT 1";
+        $result = $dbh->query($query);
+        print($result['id']);
 $dbh = null;
 
 
